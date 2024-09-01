6d123a8b04bcc797febfed3977773271a280370b
iasminsantosx <iasminwinx@gmail.com>
2024-09-01 19:05:36 -0300

    Configuração && Conexao Banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { PixMessagesModule } from './pix-messages/pix-messages.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { PixMessage } from './pix-messages/entities/pix-message.entity';
 
 @Module({
-  imports: [PixMessagesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [PixMessage],
+      synchronize: true,
+    }),
+    PixMessagesModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/pix-messages/entities/pix-message.entity.ts
+++ src/pix-messages/entities/pix-message.entity.ts
@@ -1,1 +1,42 @@
-export class PixMessage {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class PixMessage {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  endToEndId: string;
+
+  @Column('decimal')
+  valor: number;
+
+  @Column('json')
+  pagador: {
+    nome: string;
+    cpfCnpj: string;
+    ispb: string;
+    agencia: string;
+    contaTransacional: string;
+    tipoConta: string;
+  };
+
+  @Column('json')
+  recebedor: {
+    nome: string;
+    cpfCnpj: string;
+    ispb: string;
+    agencia: string;
+    contaTransacional: string;
+    tipoConta: string;
+  };
+
+  @Column({ nullable: true })
+  campoLivre: string;
+
+  @Column()
+  tx_id: string;
+
+  @Column()
+  data_hora_pagamento: Date; 
+}
